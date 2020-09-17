d3c97012be530ce6eb29f2b3ebe29a14de46a946
CodeXrook <lacekitfly@gmail.com>
2020-09-17 19:20:14 -0400

    v

---
+++ variables.js
@@ -0,0 +1,6 @@
+/*
+VARIABLES
+JavaScript variables are containers for storing data values.
+Variables are named containers that you can place data in and 
+refer to the data by naming the containers.
+ */
