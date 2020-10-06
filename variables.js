ac7de0fe130e26b767a71d3263b35753e327225e
CodeXrook <lacekitfly@gmail.com>
2020-10-06 10:47:24 -0400

    dreamChasers

--- variables.js
+++ variables.js
@@ -4,4 +4,9 @@ JavaScript variables are containers for storing data values.
 Variables are named containers that you can place data in and 
 refer to the data by naming the containers.
  */
+
+ // REVIEW
+ // variable are containers that store data
+ // must begin with upper/lower case a - z, $, _
+ // camelCase best practice
 const mac = laptop 
