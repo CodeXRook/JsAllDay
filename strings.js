fb6c74eb97b433135c8bc28e57648b18a259db5a
CodeXrook <lacekitfly@gmail.com>
2020-10-06 12:37:21 -0400

    Dr Operator

--- strings.js
+++ strings.js
@@ -29,3 +29,37 @@ greeting;// 'Good to see you!';
 
 var shoutGreeting = greeting.toUpperCase();
 shoutGreeting;//'GOOD TO SEE YOU!';
+
+// review
+// strings used to represent text
+// turn any primitive data type into a string by adding single or double quotes
+// escape backslash to handle special characters
+// length property and toUpperCase() method
+
+// challenge
+// change the following code to make valid strings
+// var animal = 'horse"; // error
+// var answer = false; // "boolean"
+// // possible solution
+// var animal = 'horse'; 
+// var answer = 'false';
+
+// challenge - fix errors
+// var greeting1 = "Sam says, "hi."";
+// var greeting2 = 'It's great to see you!';
+// // possible solution without escape character
+// greeting1 = 'Sam says, "hi."';
+// greeting2 = "It's great to see you!";
+
+// // challenge 
+// // make a valid string with escape character
+// // use \n two times to start a new line
+// var greeting3 = 'Don't forget to grab the newspaper on your way out. You are going to be on the bus for a long time because of traffic.';
+// // possible solution
+// greeting3 = 'Don\'t forget to grab the newspaper\n on your way out. You are going to be\n on the bus for a long time because of traffic.';
+
+// challenge 
+// find length of greeting3;
+// use toUpperCase() method to turn greeting3 into an uppercase string
+greeting3.length; // 120
+greeting3.toUpperCase();
