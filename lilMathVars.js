e38ed556de298699c62f987b9289c61d60d2b265
CodeXrook <lacekitfly@gmail.com>
2020-10-09 22:32:13 -0400

    lil math vars

---
+++ lilMathVars.js
@@ -0,0 +1,16 @@
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+console.log(secondsInAnHour);
+
+var hoursInADay = 24;
+var secondsInADay = secondsInAnHour * hoursInADay;
+console.log(secondsInADay);
+
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+console.log(secondsInAYear);
+
+var age = 34;
+age * secondsInAYear;
+console.log(34*secondsInAYear);
