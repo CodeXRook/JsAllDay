56c6bc1833754aa0452511cd6ef08c155320e074
CodeXrook <lacekitfly@gmail.com>
2020-10-09 21:11:33 -0400

    cat faces js

---
+++ catFaces.js
@@ -0,0 +1,9 @@
+// Draw as many cats as y ou want!
+
+var drawCats = function (howManyTimes) {
+    for (var i = 0; i <  howManyTimes; i++) {
+        console.log(i + " =^.^= ");
+    }
+};
+
+drawCats(10); // You can put any number here instead of 10.
