dd71bd363144ec183d2f459b8a784be5ceccdbb8
CodeXrook <lacekitfly@gmail.com>
2020-11-27 22:41:18 -0500

    working these function calls

--- function.js
+++ function.js
@@ -3,3 +3,26 @@ var ourFirstFunction =  function() {
 };
 
 ourFirstFunction();
+// Hello world
+
+//------------------------------
+
+var sayHelloTo = function(name) {
+    console.log("Hello my guy " + name + "!");
+};
+sayHelloTo("codeXrook");
+
+//---------------------
+// Hello my guy codeXrook!
+
+//------------------------------
+
+var printMultipleTimes = function(howManyTimes, whatToDraw) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + " " + whatToDraw);
+    }
+};
+
+printMultipleTimes(7, "|*_*|");
+
+//------------------------------
