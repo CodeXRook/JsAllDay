70ae30c0eac3652ed930d122e3ac919b5fee8be0
CodeXrook <lacekitfly@gmail.com>
2020-11-29 06:34:11 -0500

    Canceling a Timeout

--- Interactive_Programming.js
+++ Interactive_Programming.js
@@ -1,3 +1,37 @@
 // what if we want some code to run after a delay
 // or in response to something the user does?
 // Delaying Code with setTimeout
+
+/* Instead of having JavaScript execute a function immediately,
+you can tell it to execute a function after a certain period of time.
+Delaying a function like this is called setting a timeout.
+*/
+
+/* The following listing shows how we could use setTimeout to dis-
+play an alert dialog
+*/
+
+timeUp = () => {
+ alert("foreve love");
+};
+
+setTimeout(timeUp, 3000);
+
+// -----------------------------------------------------
+// Canceling a Timeout
+// To cancel a time- out, 
+// use the function clearTimeout on 
+// the timeout ID returned by setTimeout.
+
+var doHomeworkAlarm = function () {
+    alert("Hey! You need to do your homework!");
+};
+
+var timeoutId = setTimeout(doHomeworkAlarm, 6000);
+
+// To cancel the timeout, pass the timeout ID to the clearTimeout function like this:
+
+clearTimeout(timeoutId);
+
+
+// ------------------------------------------------------
