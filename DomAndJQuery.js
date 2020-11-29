6453d59cd7456a4c35a4a78dc6b9ed0dd97919c7
CodeXrook <lacekitfly@gmail.com>
2020-11-28 20:40:48 -0500

    kiss the hand of the Dom bish

---
+++ DomAndJQuery.js
@@ -0,0 +1,23 @@
+// using id to Identify elements
+
+//The HTML id attribute lets you assign a unique name, 
+// or identifier, to an HTML element
+
+<h1 id="main-heading">Hello world!</h1>
+
+//-----------------------------------------
+// Selecting an element using getelementById
+var headingElement = document.getElementById("main-heading");
+
+headingElement.innerHTML;
+
+//------------------------------------------
+
+
+//using jQuery to Work with the DoM Tree
+/*jQuery is a JavaScript library—a collection of related tools
+ (mostly func- tions) that gives us, 
+ in this case, a simpler way to work with DOM elements 
+ */
+
+ 
